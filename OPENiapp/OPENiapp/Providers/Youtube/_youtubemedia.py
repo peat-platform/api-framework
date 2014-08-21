c864eb488fe50a482ea0c5b9dc32ca746d4de49d
Miggi Zwicklbauer <mzw@fokus.fraunhofer.de>
2014-08-21 15:09:36 +0200

    #youtube get all videos for account

--- OPENiapp/OPENiapp/Providers/Youtube/_youtubemedia.py
+++ OPENiapp/OPENiapp/Providers/Youtube/_youtubemedia.py
@@ -77,7 +77,76 @@ class youtubeMedia(bcMedia):
 
     def get_all_videos_for_account(self, params):
         """ GET API_PATH/[ACCOUNT_ID]/videos """
-        return defaultMethodResponse
+        channels_response = self.connector.channels().list(
+          mine=True,
+          part="contentDetails"
+        ).execute()
+        
+        for channel in channels_response["items"]:
+          uploads_list_id = channel["contentDetails"]["relatedPlaylists"]["uploads"]
+          
+        playlistitems_list_request = self.connector.playlistItems().list(
+            playlistId=uploads_list_id,
+            part="snippet",
+            maxResults=50
+        )
+    
+        playlistitems_list_response = playlistitems_list_request.execute()
+        
+        i=0
+        
+        names = ['id', 'object_type', 'service', 'url', 'from_id', 'from_object_type', 'from_url', 'from_name', 'time_created_time', 'time_edited_time', 'time_deleted_time']
+        names.extend(['file_title', 'file_description', 'file_format', 'file_size', 'file_icon'])
+        names.extend(['location_latitude', 'location_longitude', 'location_height'])
+        names.extend(['duration_starts_time', 'duration_ends_time'])
+        names.extend(['tags'])
+
+        fields = ['id', 'object_type', 'service', '', '', '', '', '', 'recordingDetails.recordingDate', '', '']
+        fields.extend(['snippet.title', 'snippet.description', 'fileDetails.fileType', 'fileDetails.fileSize', 'thumbnails.default.url'])
+        fields.extend(['recordingDetails.location.latitude', 'recordingDetails.location.longitude', 'recordingDetails.location.altitude'])
+        fields.extend(['', 'contentDetails.duration'])
+        fields.extend(['snippet.tags'])
+
+        alternatives = ['', 'video', 'youtube', '', '', '', '', '', '', '', '']
+        alternatives.extend(['', '', '', '', ''])
+        alternatives.extend(['', '', ''])
+        alternatives.extend(['', ''])
+        alternatives.extend([''])
+        
+        response = {
+                    'meta':
+                        {
+                         'total_count': [],
+                         'previous': defJsonRes,
+                         'next': defJsonRes
+                        },
+                    'data': []
+                    }
+    
+        for playlist_item in playlistitems_list_response["items"]:
+            metadata = self.connector.videos().list(id =playlist_item["snippet"]["resourceId"]["videoId"], part = "id, snippet, recordingDetails, fileDetails").execute()
+            raw_datas = metadata['items'][0]
+  
+            for raw_data in metadata.get("items", []):
+                data = self.get_fields(raw_datas, names, fields, alternatives)
+                response['data'].append(self.format_video_response(data))
+                i +=1
+                print response
+
+            tag_array = []
+            if (check_if_exists(raw_datas, 'snippet.tags') != defJsonRes):
+                for tag in metadata.get("items", []):
+                        tag_names = ['tags_id', 'tags_name', 'tags_time_created_time', 'tags_time_edited_time', 'tags_time_deleted_time', 'tags_x-location', 'tags_y-location']
+                        tag_fields = ['', 'snippet.tags' , '', '', '', '', '']
+                        tag_alternatives = ['', '', '', '', '', '', '']
+                        tag_data = self.get_fields(tag, tag_names, tag_fields, tag_alternatives)
+                        tag_array.append(format_tags(tag_data))
+                response['data'][0]['tags'] = tag_array  
+                
+            response['meta']['total_count'] = i   
+
+        return response
+          
 
     def post_video_to_account(self, params):
         """ POST API_PATH/[ACCOUNT_ID]/videos """
@@ -107,32 +176,32 @@ class youtubeMedia(bcMedia):
         return "Insufficient Parameters"
             
     def post_video_to_aggregation(self, params):
-        """ POST API_PATH/[AGGREGATION_ID]/videos """
-        #account == users channel
-        if (check_if_exists(params, 'source') != defJsonRes):
-            tags = None
-            if params['keywords']:
-                tags = params['keywords'].split(",")
-            
-            body=dict(
-                snippet=dict(
-                  title= params['title'],
-                  description= params['description'],
-                  tags=tags
-                ),
-                status=dict(
-                  privacyStatus= params['privacyStatus']
-                )
-              )
-            
-            # Call the API's videos.insert method to create and upload the video.
-            return self.connector.videos().insert(
-                part=",".join(body.keys()),
-                body=body,
-                media_body=MediaFileUpload(params['source'], chunksize=-1, resumable=True)
-              ).execute()
-            
-        return "Insufficient Parameters"
+        """ POST API_PATH/[AGGREGATION_ID]/videos """        
+        #youtube playlist -> aggregation
+#         if (check_if_exists(params, 'source') != defJsonRes):
+#             tags = None
+#             if params['keywords']:
+#                 tags = params['keywords'].split(",")
+#             
+#             body=dict(
+#                 snippet=dict(
+#                   title= params['title'],
+#                   description= params['description'],
+#                   tags=tags
+#                 ),
+#                 status=dict(
+#                   privacyStatus= params['privacyStatus']
+#                 )
+#               )
+#             
+#             # Call the API's videos.insert method to create and upload the video.
+#             return self.connector.playlistsItem().insert(
+#                 part=",".join(body.keys()),
+#                 body=body,
+#                 media_body=MediaFileUpload(params['source'], chunksize=-1, resumable=True)
+#               ).execute()
+#             
+#         return "Insufficient Parameters"
 
     def edit_a_video(self, params):
         """ PUT API_PATH/[VIDEO_ID] """
