8e1768dee1dba2ef2116a9a68dc32c31395300d8
romanos <rtsouroplis@epu.ntua.gr>
2014-08-25 16:06:47 +0300

    Minor

--- OPENiapp/OPENiapp/Providers/Google/_goactivity.py
+++ OPENiapp/OPENiapp/Providers/Google/_goactivity.py
@@ -1,7 +1,7 @@
 from OPENiapp.Providers.base.activity import bcActivity
 from OPENiapp.Providers.base.common import *
 
-class goAvtivity(bcActivity):
+class goActivity(bcActivity):
 
     def get_an_event(self, params):
         """ GET API_PATH/[EVENT_ID] """
