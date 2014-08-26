9b79642bfa25241ebefe71b41c2ea60f15472db5
romanos <rtsouroplis@epu.ntua.gr>
2014-08-26 17:04:20 +0300

    Google Places Checkin - Checkin Object in OPENi
    Fixed #35

--- OPENiapp/OPENiapp/Providers/Google/_goactivity.py
+++ OPENiapp/OPENiapp/Providers/Google/_goactivity.py
@@ -3,6 +3,7 @@ from OPENiapp.Providers.base.common import *
 
 class goActivity(bcActivity):
 
-    def get_an_event(self, params):
-        """ GET API_PATH/[EVENT_ID] """
-        return defaultMethodResponse
+    def post_checkin_to_account(self, params):
+        """ GET API_PATH/[CHECKIN_ID] """
+        # /reference (ie /CnRsAAAA98C4wD-VFvzGq-KHVEFhlHuy1TD1W6UYZw7KjuvfVsKMRZkbCVBVDxXFOOCM108n9PuJMJxeAxix3WB6B16c1p2bY1ZQyOrcu1d9247xQhUmPgYjN37JMo5QBsWipTsnoIZA9yAzA-0pnxFM6yAcDhIQbU0z05f3xD3m9NQnhEDjvBoUw-BdcocVpXzKFcnMXUpf-nkyF1w)
+        return self.connector.checkin(params['reference'])
