267033cc0c6be532074e92e2facdc6f9e49c6ea1
Michael Petychakis <mpetyx@gmail.com>
2014-09-09 00:30:59 +0300

    os.environ['HTTPS'] = "on"

--- OPENiapp/configs/settings_local.py
+++ OPENiapp/configs/settings_local.py
@@ -7,6 +7,7 @@ import os
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+os.environ['HTTPS'] = "on"
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
