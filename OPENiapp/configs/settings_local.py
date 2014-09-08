bfe025ffb8fb510b153616e795dbd1d2b7467c55
Michael Petychakis <mpetyx@gmail.com>
2014-09-09 00:36:57 +0300

    APPEND_SLASH at the end of the request

--- OPENiapp/configs/settings_local.py
+++ OPENiapp/configs/settings_local.py
@@ -8,6 +8,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 os.environ['HTTPS'] = "on"
+APPEND_SLASH = False
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
