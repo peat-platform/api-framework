8541496ef067666f89c2b889433bf9553944af0c
romanos <rtsouroplis@epu.ntua.gr>
2014-09-30 16:53:12 +0300

    Fixed imports

--- OPENiapp/OPENiapp/APIS/Profile/Account/models.py
+++ OPENiapp/OPENiapp/APIS/Profile/Account/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from OPENiapp.APIS.commonModels import *
 
-from OPENiapp.APIS.Products_and_services.Wallet import OpeniWallet
-from OPENiapp.APIS.Profile.User import OpeniUser
+from OPENiapp.APIS.Products_and_services.Wallet.models import OpeniWallet
+from OPENiapp.APIS.Profile.User.models import OpeniUser
 
 class OpeniAccount(GenericModel):
     Person = models.ForeignKey(PersonModel)
