32c20da9352afcfc44e5c6aa91aebe2dc193252e
Michael Petychakis <mpetyx@gmail.com>
2014-09-30 17:08:11 +0300

    Location API Completed

--- OPENiapp/OPENiapp/APIS/Location/Route/Resources.py
+++ OPENiapp/OPENiapp/APIS/Location/Route/Resources.py
@@ -11,13 +11,4 @@ from OPENiapp.APIS.OPENiAuthentication import Authentication
 class RouteResource(GenericResource):
     class Meta(GenericMeta):
         queryset = OpeniRoute.objects.all()
-        list_allowed_methods = ['get', 'post']
-        detail_allowed_methods = ['get', 'post', 'put', 'delete']
         resource_name = 'Route'
-        authentication = Authentication()
-        authorization = Authorization()
-        # filtering = {
-        #     'slug': ALL,
-        #     'user': ALL_WITH_RELATIONS,
-        #     'created': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
-        # }
