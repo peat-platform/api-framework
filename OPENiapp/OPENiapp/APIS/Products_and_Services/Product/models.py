98599fd30302a2b397bbe9e162843c3b74c676d0
Iosif Alvertis <alvertisjo@gmail.com>
2014-09-30 13:00:19 +0300

    orders and products updated

--- OPENiapp/OPENiapp/APIS/Products_and_Services/Product/models.py
+++ OPENiapp/OPENiapp/APIS/Products_and_Services/Product/models.py
@@ -6,14 +6,25 @@ __author__ = 'mpetyx'
 from django.db import models
 
 
+
+
+class ProductProfile(models.Model):
+    name = models.TextField() #The name of the product		string
+    description = models.TextField()	#Any description for a product		string
+    category = models.TextField()	#The categories the product may belong		list of strings
+    picture = models.TextField()	#A picture of the product		string
+    #company	The company that produces the product		An [[Profile#Organization
+    year = models.IntegerField()	#The year the product became available for the first time		string
+
+
 class OpeniProduct(OpeniContextAwareModel):
     # id is missing because it is the default
     url = models.TextField()
     object_type = models.TextField()
     service = models.TextField()
     From = models.TextField()
-    price = models.TextField()
+    price = models.FloatField()
     currency = models.TextField()
     amount = models.TextField()
     code = models.TextField()
-    profile = models.TextField()
+    profile = models.ForeignKey(ProductProfile)
