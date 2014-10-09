bb6b453dbd928fffdbf86f04852ce999d2d87c0b
romdim <rom-dim@hotmail.com>
2014-10-09 16:16:03 +0300

    Fixed #40
    Iosif had removed the connector file for some reason.
    Brought it back and removed it from gitignore.
    This was made at commit: 54176cd75e9c53b0205de2419b7f7b6b10203665

---
+++ OPENiapp/OPENiapp/Providers/Vimeo/connector.py
@@ -0,0 +1,18 @@
+# Include the Dropbox SDK
+from vimeo import VimeoClient
+#from dropbox import client, rest, session
+from OPENiapp.Providers.baseConnector import basicProvider
+
+from _vimeoactivity import vimeoActivity
+from _vimeolocation import vimeoLocation
+from _vimeomedia import vimeoMedia
+from _vimeoproductsServices import vimeoProductsServices
+from _vimeoprofiles import vimeoProfiles
+
+class provider(basicProvider, vimeoActivity, vimeoLocation, vimeoMedia, vimeoProductsServices, vimeoProfiles):
+    """ This class is used to:
+        1. Make the connection to the vimeo
+    """
+    def __init__(self, access_token):#, data):
+        """ Initiate the graph and find the OPENi album """
+        self.connector = VimeoClient(access_token=access_token)
