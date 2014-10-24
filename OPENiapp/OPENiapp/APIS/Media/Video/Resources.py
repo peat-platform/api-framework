5237715f6b6d61b9cea4e44ef2eac3a10a58ac2c
Michael Petychakis <mpetyx@gmail.com>
2014-10-24 11:14:05 +0300

    media API updated and finalised

--- OPENiapp/OPENiapp/APIS/Media/Video/Resources.py
+++ OPENiapp/OPENiapp/APIS/Media/Video/Resources.py
@@ -10,13 +10,172 @@ from OPENiapp.APIS.OPENiAuthentication import Authentication
 class VideoResource(GenericResource):
     class Meta(GenericMeta):
         queryset = OpeniVideo.objects.all()
-        list_allowed_methods = ['get', 'post']
-        detail_allowed_methods = ['get', 'post', 'put', 'delete']
         resource_name = 'Video'
-        authentication = Authentication()
-        authorization = Authorization()
-        # filtering = {
-        #     'slug': ALL,
-        #     'user': ALL_WITH_RELATIONS,
-        #     'created': ['exact', 'range', 'gt', 'gte', 'lt', 'lte'],
-        # }
+        
+        extra_actions = [
+            {
+                "name": "",
+                "http_method": "GET",
+                "summary": "Retrieve all user Videos from CBS",
+                "resource_type": "list",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "",
+                "http_method": "DELETE",
+                "summary": "Delete a user Video from CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "Videos",
+                "http_method": "GET",
+                "summary": "Retrieve all user Videos from CBS",
+                "resource_type": "list",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "",
+                "http_method": "POST",
+                "summary": "Post a new Video on CBS",
+                "resource_type": "list",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    },
+
+                }
+            },
+            {
+                "name": "Commments",
+                "http_method": "POST",
+                "summary": "Post a new comment on target Video on CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "Commments",
+                "http_method": "GET",
+                "summary": "GET comments from Video on CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "Likes",
+                "http_method": "POST",
+                "summary": "Post a new like on target Video on CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "Likes",
+                "http_method": "GET",
+                "summary": "GET likes from Video on CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            },
+            {
+                "name": "Likes",
+                "http_method": "DELETE",
+                "summary": "Delete likes from Video on CBS",
+                "fields": {
+                    "user": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Current user"
+                    },
+                    "cbs": {
+                        "type": "string",
+                        "required": False,
+                        "description": "Facebook"
+                    }
+
+                }
+            }
+        ]
