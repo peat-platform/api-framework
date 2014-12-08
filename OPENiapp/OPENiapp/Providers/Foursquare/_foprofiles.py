2fb0726329b31490e9f254ba22122c377a5676bb
romdim <rom-dim@hotmail.com>
2014-12-08 16:22:33 +0200

    Fixed foursquare same way

--- OPENiapp/OPENiapp/Providers/Foursquare/_foprofiles.py
+++ OPENiapp/OPENiapp/Providers/Foursquare/_foprofiles.py
@@ -8,7 +8,7 @@ class foProfiles(bcProfiles):
     #   region Profiles API
     #   As described here: https://opensourceprojects.eu/p/openi/wiki/Profiles%20API/
 
-    def get_account(self):
+    def get_accounts(self):
         return self.get_an_account('self')
     
     def get_an_account(self, id):
@@ -18,13 +18,13 @@ class foProfiles(bcProfiles):
         names = ['id', 'object_type', 'service', 'resource_uri', 'from_id', 'from_object_type', 'from_resource_uri', 'from_name', 'time_created_time', 'time_edited_time', 'time_deleted_time']
         names.extend(['person_name', 'person_surname', 'person_middlename', 'person_birthdate'])
         names.extend(['wallet_cards', 'wallet_name', 'wallet_description',
-                      'wallet_id', 'wallet_object_type', 'wallet_service', 'wallet_url', 'wallet_from_id', 'wallet_from_object_type', 'wallet_from_url', 'wallet_from_name', 'wallet_time_created_time', 'wallet_time_edited_time', 'wallet_time_deleted_time'])
+                      'wallet_id', 'wallet_object_type', 'wallet_service', 'wallet_resource_uri', 'wallet_from_id', 'wallet_from_object_type', 'wallet_from_url', 'wallet_from_name', 'wallet_time_created_time', 'wallet_time_edited_time', 'wallet_time_deleted_time'])
         names.extend(['user', 'username', 'email', 'password', 'validated', 'active'])
 
         fields = ['id', 'type', 'service', 'link', 'id', 'type', 'link', 'firstName', 'createdAt', 'time.updated_time', 'time.deleted_time']
         fields.extend(['firstName', 'lastName', '', ''])
         fields.extend(['wallet_cards', 'wallet_name', 'wallet_description',
-                      'wallet_id', 'wallet_object_type', 'wallet_service', 'wallet_url', 'wallet_from_id', 'wallet_from_object_type', 'wallet_from_url', 'wallet_from_name', 'wallet_time_created_time', 'wallet_time_edited_time', 'wallet_time_deleted_time'])
+                      'wallet_id', 'wallet_object_type', 'wallet_service', 'wallet_resource_uri', 'wallet_from_id', 'wallet_from_object_type', 'wallet_from_url', 'wallet_from_name', 'wallet_time_created_time', 'wallet_time_edited_time', 'wallet_time_deleted_time'])
         fields.extend(['user', 'username', 'email', 'password', 'verified', 'active'])
 
         alternatives = ['', 'account', 'foursquare', '', '', 'person', '', '', '', '', '']
